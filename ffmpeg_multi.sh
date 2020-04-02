81896f0f1a7f2f9c21e52aae8205cfda1049972a
0rcun <orcunkamiloglu@gmail.com>
2020-04-02 09:18:55 +0300

    Add files via upload

---
+++ ffmpeg_multi.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+///ffmpeg_multi.sh m4a mp3 ./Desktop/musiccc/ ./Desktop/mp3/
+srcExt=$1
+destExt=$2
+
+srcDir=$3
+destDir=$4
+
+opts=$5
+
+for filename in "$srcDir"/*.$srcExt; do
+
+        basePath=${filename%.*}
+        baseName=${basePath##*/}
+
+        ffmpeg -i "$filename" $opts "$destDir"/"$baseName"."$destExt"
+
+done
+
+echo "Conversion from ${srcExt} to ${destExt} complete!"
